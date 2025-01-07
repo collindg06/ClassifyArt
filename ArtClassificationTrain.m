c7ab00ecd45580cb1427fe765be3df284b273b73
Collin <collindg06@gmail.com>
2025-01-07 14:15:04 -0500

    Add existing file

---
+++ ArtClassificationTrain.m
@@ -0,0 +1,85 @@
+% Ensure the net.mat file and art folder are in your current directory
+% This code trains and tests a neural network that can decipher between real art and ai art
+load('net.mat');
+
+% create image data store of all pictures
+store = imageDatastore("Art\",IncludeSubfolders=true,LabelSource="foldernames");
+
+% seperate train/test data and resize
+[storeTrain,storeTest] = splitEachLabel(store,.5);
+storeTrainRe = augmentedImageDatastore([224 224],storeTrain,'ColorPreprocessing', 'gray2rgb');
+storeTestRe = augmentedImageDatastore([224 224],storeTest, 'ColorPreprocessing', 'gray2rgb');
+
+% preallocation and initialization
+group = 1;
+f1Score = zeros(2,1);
+
+% loop for n-fold cross validation
+while (group < 3)
+
+    % train network depending upon the group
+    opts = trainingOptions("sgdm");
+    if (group == 1)
+
+        [train,labels] = trainNetwork(storeTrainRe, lgraph_1, opts);
+        % predict image classification
+        predictions = classify(train, storeTestRe);
+        actual = storeTest.Labels;
+
+    elseif (group == 2)
+
+        [train,labels] = trainNetwork(storeTestRe, lgraph_1, opts);
+        % predict image classification
+        predictions = classify(train, storeTrainRe);
+        actual = storeTrain.Labels;
+
+    end
+
+    % comparing the correct labels with the predictions
+    correct = (predictions == actual);
+
+    % preallocation and initialization
+    correctLabels = strings(1000,1);
+    falseLabels = strings(1000,1);
+    counterCorrect = 1;
+    counterFalse  = 1;
+
+    % loop to get correct and false labels
+    for x = 1 :length(predictions)
+        if (correct(x) == 1)
+            correctLabels(counterCorrect) = predictions(x);
+            counterCorrect = counterCorrect + 1;
+    
+        elseif (correct(x) == 0)
+            falseLabels(counterFalse) = predictions(x);
+            counterFalse = counterFalse + 1;
+        end
+    end 
+
+    % get the amount of correct predictions
+    numCorrect = nnz(correct);
+
+    % calculate true positive, false positive, true negative, and false negative
+    truePositive = nnz(correctLabels == "Real");
+    falsePositive = nnz(falseLabels == "Real");
+    trueNegative = nnz(correctLabels == "Ai");
+    falseNegative = nnz(falseLabels == "Ai");
+
+    % calculate precision, recall, and F1 score
+    precision = truePositive / (truePositive + falsePositive) ;
+    recall = truePositive / (truePositive + falseNegative);
+    f1Score(group) = 2 * ((precision * recall) / (precision + recall));
+
+    % print out
+    disp (strcat("Group " , num2str(group)))
+    disp (strcat("Precision = " , num2str(precision)));
+    disp (strcat("Recall = " , num2str(recall)));
+    disp (strcat("F1 score = " , num2str(f1Score(group))));
+    
+    group = group +1;
+
+end
+
+% calculate and display the average f1 score
+averagePerformance = (f1Score(1) + f1Score(2)) / 2;
+disp(strcat("Average performance of model in terms of F1 score: ", num2str(averagePerformance)))
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# ClassifyArt
+# Art Classifification
+
+## Programs that train a model to determine whether a piece of art was made by a person or AI and then allows the user to input an image to detect if it was made using AI or not.
+
+## Requirements
+
+- Must have MATLAB downloaded.
+- Must have MATLAB’s directory in ClassifyArt folder.
+- Must have any image you want to classify in the folder as well.
+
+## Instructions to run
+
+- Open ArtClassificationTrain.m and run the file in the editor tab.
+- Open ArtClassification.m and run the file.
+- Enter a file name that is a jpg or png file in the command window (you can enter “aiimage.jpg” if you do not have one).
+- The program will then give you the classification.
